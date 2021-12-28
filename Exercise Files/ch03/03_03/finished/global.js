d20e7dac889550f0a3b3deecad44fc34abcb62f3
Al3nMicL <al3nmicl@users.noreply.github.com>
2021-12-28 03:54:25 +0000

    argument variables with process.argv

--- Exercise Files/ch03/03_03/finished/global.js
+++ Exercise Files/ch03/03_03/finished/global.js
@@ -1,3 +1,18 @@
-const path = require("path");
+//const path = require("path");
 
-console.log(`The file name is ${path.basename(__filename)}`);
+//console.log(`The file name is ${path.basename(__filename)}`);
+//const [, , firstname, lastname] = process.argv;
+// console.log(`Your name is ${firstname} ${lastname}`);
+
+const grab = flag => {
+    let  indexAfterFlag = process.argv.indexOf(flag) + 1;
+    return process.argv[indexAfterFlag];
+}
+
+const greeting = grab("--greeting");
+const user = grab("--user");
+
+
+console.log(process.argv);
+console.log(`${greeting} ${user}!`);
+// now type in terminal: node global.js --user "Yourname" --greeting "Hey Wasup, "
