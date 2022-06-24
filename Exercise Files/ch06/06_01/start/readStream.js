ca052efcc31a0b7a1f7629b2aa432dded823579c
Al3nMicL <al3nmicl@users.noreply.github.com>
2022-06-23 22:20:39 -0400

    readable file streams

--- Exercise Files/ch06/06_01/start/readStream.js
+++ Exercise Files/ch06/06_01/start/readStream.js
@@ -1,4 +1,22 @@
-console.log("type something...");
-process.stdin.on("data", data => {
-  console.log(`I read ${data.length - 1} characters of text`);
+const fs = require("fs");
+
+let filePath = "./assets/lorum-ipsum.md"; 
+let encoding = "UTF-8";
+let fileTxt = "";
+const readStream = fs.createReadStream(filePath, encoding);
+
+// console.log("type something...");
+
+readStream.on("data", data => {
+  // console.log(`I read ${data.length - 1} characters of text`);
+  fileTxt += data;
+});
+
+readStream.once("data", data => {
+  console.log(data);
+});
+
+readStream.on("end", () =>{
+  console.log("\nEnd of stream data.");
+  console.log(`Stream was ${fileTxt.length - 1} characters long.`);
 });
