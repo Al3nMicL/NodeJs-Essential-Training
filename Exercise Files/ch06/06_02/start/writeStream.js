068adf6cc77c8c3bba02a0d8331e388448acbb60
Al3nMicL <al3nmicl@users.noreply.github.com>
2022-07-27 22:51:58 -0400

    writable file streams

---
+++ Exercise Files/ch06/06_02/start/writeStream.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+let filePath = "./assets/myFile.md";
+let encoding = "UTF-8";
+
+const writeStream = fs.createWriteStream(filePath, encoding);
+
+writeStream.write("# Hello World! ");
+writeStream.write("## :wave:  ");
+
+console.log("file written successfully... \n");
